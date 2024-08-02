af58a4fcae2ed8ff438bc04baf3ea732df94dfae
TrietIT1210 <165779386+TrietIT1210@users.noreply.github.com>
2024-08-02 16:20:24 +0700

    Add files via upload

---
+++ src/main/java/com/ourproject/auctionsystem/JewelryAuctionSystemApplication.java
@@ -0,0 +1,10 @@
+package com.ourproject.auctionsystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryAuctionSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JewelryAuctionSystemApplication.class, args);
+    }
+}
